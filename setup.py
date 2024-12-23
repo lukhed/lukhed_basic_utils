dfffca2c19839c9da2e04bc9779f62669d45458e
lukhed <heideluk7@gmail.com>
2024-12-23 17:55:46 -0500

    27 add requets common (#28)
    * added new packages
    
    * add imports to reqeustsCommon
    
    * added request functions and additional dependencies
    
    ---------
    
    Co-authored-by: lukhed <lukhed.mail@gmail.com>

--- setup.py
+++ setup.py
@@ -19,5 +19,8 @@ setup(
     install_requires=[
         "pytz>=2024.2",
         "python-dateutil>=2.9.0",
+        "requests>=2.32.3",
+        "beautifulsoup4>=4.12.3",
+        "fake-useragent>=2.0.3"
     ],
 )
