f736e8268ce458b94f6bc2a1c2d2dc1f47e79fb3
lukhed <lukhed.mail@gmail.com>
2024-12-23 12:52:00 -0500

    update version to 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="aceCommon",
-    version="0.1.0",
+    version="0.1.1",
     description="A collection of common utility functions",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
