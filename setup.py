e89d822eaf219b9b0bc9e63a73a82c750e099c9b
lukhed <lukhed.mail@gmail.com>
2025-06-01 11:22:04 -0400

    upped version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="lukhed_basic_utils",
-    version="1.5.0",
+    version="1.6.0",
     description="A collection of basic utility functions",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
